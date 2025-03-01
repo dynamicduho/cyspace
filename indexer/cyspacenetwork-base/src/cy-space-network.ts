724514602ed87790ab749fa52791425f2b6f9126
pane2004 <alexlu2000clash@gmail.com>
2025-02-28 18:27:27 -0700

    set up indexer for photo albums

--- indexer/cyspacenetwork-base/src/cy-space-network.ts
+++ indexer/cyspacenetwork-base/src/cy-space-network.ts
@@ -1,47 +1,17 @@
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import {
-  AdminAdded as AdminAddedEvent,
-  AdminRemoved as AdminRemovedEvent,
+  CySpaceNetwork as CySpaceNetworkContract,
   DiaryCreated as DiaryCreatedEvent,
   FriendAdded as FriendAddedEvent,
   FriendRemoved as FriendRemovedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
   PhotoCreated as PhotoCreatedEvent
 } from "../generated/CySpaceNetwork/CySpaceNetwork"
 import {
-  AdminAdded,
-  AdminRemoved,
   DiaryCreated,
   FriendAdded,
   FriendRemoved,
-  OwnershipTransferred,
-  PhotoCreated
+  PhotoAlbum
 } from "../generated/schema"
-
-export function handleAdminAdded(event: AdminAddedEvent): void {
-  let entity = new AdminAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.admin = event.params.admin
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleAdminRemoved(event: AdminRemovedEvent): void {
-  let entity = new AdminRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.admin = event.params.admin
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
 
 export function handleDiaryCreated(event: DiaryCreatedEvent): void {
   let entity = new DiaryCreated(
@@ -86,33 +56,38 @@ export function handleFriendRemoved(event: FriendRemovedEvent): void {
   entity.save()
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
 export function handlePhotoCreated(event: PhotoCreatedEvent): void {
-  let entity = new PhotoCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.author = event.params.author
-  entity.caption = event.params.caption
-  entity.timestamp = event.params.timestamp
+  let cyNetwork = CySpaceNetworkContract.bind(event.address);
+  let index = BigInt.fromI32(0);
+  let contentAddress = new Address(0);
+  while (true) {
+    let result = cyNetwork.try_photos(event.params.author, index);
+    if (result.reverted) {
+      break;
+    }
+    let photo = result.value;
+    let possibleTimestamp = photo.getTimestamp();
+    if (possibleTimestamp.equals(event.block.timestamp)) {
+      // found a match: store it and stop iterating
+      contentAddress = photo.getContentAddress();
+      break;
+    }
+    index = index.plus(BigInt.fromI32(1));
+  }
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  if (contentAddress !== new Address(0)) {
+    let entity = new PhotoAlbum(
+      event.transaction.hash.concatI32(event.logIndex.toI32())
+    );
+    entity.author = event.params.author;
+    entity.caption = event.params.caption;
+    entity.timestamp = event.params.timestamp;
+    entity.fileDirectory = contentAddress;
 
-  entity.save()
+    entity.blockNumber = event.block.number;
+    entity.blockTimestamp = event.block.timestamp;
+    entity.transactionHash = event.transaction.hash;
+
+    entity.save();
+  }
 }
