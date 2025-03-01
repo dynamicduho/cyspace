a5fbaadec3f857913b044a81fbadaa20eb152876
Joshua Duho Kim <joshua.kim@uwaterloo.ca>
2025-03-01 04:48:49 -0700

    Sort photo posts in reverse chronological order

--- frontend/src/query/photoalbums.ts
+++ frontend/src/query/photoalbums.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_ALL_PHOTO_ALBUMS = gql`
   query GetAlbums {
-    photoAlbums {
+    photoAlbums(orderBy: timestamp, orderDirection: desc) {
       id
       author
       caption
